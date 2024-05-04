485faf54a31eec2ccd825d2c6a4201ce18f285c9
Neko Box Coder <nekoboxcoder@gmail.com>
2024-05-04 18:37:41 +0100

    Update script

--- ChooseLatestFolder.sh
+++ ChooseLatestFolder.sh
@@ -3,8 +3,13 @@ set -e
 # <First folder to compare> <Second folder to compare> <Destination folder>
 
 if [ -d $1 ] && [ -d $2 ]; then
-    mainTime=$(cat $1/.pipeline_start_time)
-    prTime=$(cat $2/.pipeline_start_time)
+    mainTime=$(cat $1/.pipeline_start_time || echo 0)
+    prTime=$(cat $2/.pipeline_start_time || echo 0)
+
+    if [ mainTime -eq 0 ] && [ prTime -eq 0 ]; then
+        echo "Both times are missing"
+        exit 1
+    fi
 
     if  [ $mainTime -ge $prTime ]; then
         mv $1 $3
